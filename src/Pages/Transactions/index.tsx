63f9f31f4001fbfb97bf40be04ebd146a53bd035
mariaemf <mariaeduardamartinsf@gmail.com>
2022-10-13 21:26:31 -0300

    Construção de tabela de transações

--- src/Pages/Transactions/index.tsx
+++ src/Pages/Transactions/index.tsx
@@ -1,11 +1,39 @@
 import { Header } from "../../Components/Header/index";
 import { Summary } from "../../Components/Summary";
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de Site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000.00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/10/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 59,99</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>12/10/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
