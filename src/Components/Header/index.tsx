aaf003f48ab8e8a3f2b5b54fa0ad4935402efc29
mariaemf <mariaeduardamartinsf@gmail.com>
2022-10-17 20:44:44 -0300

    Criação de modal acessível - Estrutura visual

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,12 +1,26 @@
 import logoImg from "../../Assets/logo.svg";
-import { HeaderContainer, HeaderContent, NewTransactionButton } from "./Styles";
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import * as Dialog from "@radix-ui/react-dialog";
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt="" />
-        <NewTransactionButton> Nova Transação </NewTransactionButton>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton> Nova Transação </NewTransactionButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+            <Dialog.Content>
+              <Dialog.Title> Nova Transação</Dialog.Title>
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
