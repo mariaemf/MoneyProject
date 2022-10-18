aad0e4b117fb2d835599bf38704cec0c9e4ac9dd
mariaemf <mariaeduardamartinsf@gmail.com>
2022-10-17 21:35:52 -0300

    modal de nova transição realizado com estilização do mesmo

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,6 +1,7 @@
 import logoImg from "../../Assets/logo.svg";
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import * as Dialog from "@radix-ui/react-dialog";
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
   return (
@@ -12,14 +13,7 @@ export function Header() {
           <Dialog.Trigger asChild>
             <NewTransactionButton> Nova Transação </NewTransactionButton>
           </Dialog.Trigger>
-
-          <Dialog.Portal>
-            <Dialog.Overlay />
-            <Dialog.Content>
-              <Dialog.Title> Nova Transação</Dialog.Title>
-              <Dialog.Close />
-            </Dialog.Content>
-          </Dialog.Portal>
+          <NewTransactionModal />
         </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
